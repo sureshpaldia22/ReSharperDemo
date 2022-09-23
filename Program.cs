2b407afe27370fe6321761b47f160c8424c1d5d7
Suresh Paldia <spaldia@eclinicalsol.com>
2022-09-23 16:32:29 -0400

    ok

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ReSharper_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var test1 = new TestUnreachableCode();
+            test1.Test();
+        }
+    }
+}
