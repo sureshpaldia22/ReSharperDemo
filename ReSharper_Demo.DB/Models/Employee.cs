53a8ddea0ab3a3dbb19e6480862c3fee2b7724c2
Suresh Paldia <spaldia@eclinicalsol.com>
2022-09-23 16:37:02 -0400

    ok

---
+++ ReSharper_Demo.DB/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReSharper_Demo.DB.Models
+{
+    public partial class Employee
+    {
+        // TODO: what?
+        public int? Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ReSharper_Demo.DB/Models/ReSharper_DemoContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ReSharper_Demo.DB.Models
+{
+    public partial class ReSharper_DemoContext : DbContext
+    {
+        public ReSharper_DemoContext()
+        {
+        }
+
+        public ReSharper_DemoContext(DbContextOptions<ReSharper_DemoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ReSharper_Demo;Trusted_Connection=False;Integrated Security=True");
+            }
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
